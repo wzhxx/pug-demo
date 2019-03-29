8493216479bf9c04a6dc5c9f2af3aeb547c21100
wzhxx <wzhxxx123@163.com>
2019-03-29 16:31:18 +0800

    1st commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,32 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+
+	div.hello
+		h1 {{msg}}
+		p For a guide and recipes on how to configure | customize this project,check out the
+			a(href="https://cli.vuejs.org" target="_blank" rel="noopener") vue-cli documentation
+		h3 Installed CLI plugins
+		ul: li
+			a(href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener") babel
+			a(href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener") eslint
+		h3 Essential Links
+			ul: li(v-for="item in essentialLink"): a(:href="item.url" target="_blank" rel="noopener") {{item.name}}
+		h3 Ecosystem
+			ul
+				li(v-for="item in ecosystem"): a(:href="item.url" target="_blank" rel="noopener") {{item.name}}
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+	data(){
+		return{
+			essentialLink: [{url:'https://vuejs.org',name: 'Core Docs'},{url:'https://chat.vuejs.org',name: 'Community Chat'},{url:'https://twitter.com/vuejs',name: 'Twiter'},{url:'https://news.vuejs.org',name: 'News'}],
+			ecosystem:	[{url:'https://router.vuejs.org',name:'vue-router'},{url:'https://vuex.vuejs.org',name:'vuex'},{url:'https://github.com/vuejs/vue-devtools#vue-devtools',name: 'vue devtools'},{url:'https://vue-loader.vuejs.org',name:'vue-loader'},{url:'https://github.com/vuejs/awesome-vue',name:'awesome-vue'}]
+		}
+	}
 }
 </script>
 
